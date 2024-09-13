52845b116ebade3aea41afd75effb428e56ed434
TalhaSyed65737 <talhauamr397@gmail.com>
2024-09-12 22:05:03 -0700

    add files

---
+++ 13.js
@@ -0,0 +1,14 @@
+let num = Math.random();
+console.log(Math.round(6.6));
+
+let num2 = Math.random();
+console.log(Math.ceil(2.2));
+
+// ceil mean celling it will take th upper number. eg-  
+// 2.3 = 3,  3.4 = 4, etc,
+
+let num3= Math.random();
+
+console.log(Math.floor(2.9));
+// floor will take the lower number eg: 3.9 = 3, 4.9=4
+
--- 9.js
+++ 9.js
@@ -1,11 +1,11 @@
-let num= prompt("Enter the Number");
+// let num= prompt("Enter the Number");
 
-if(num ==  ){
-    alert("the given number is positive");
-}
-else if(num == negative){
-    alert("the give number is negative");
-}
-else{
-    alert("the given number is zero");
-}
+// if( num == 2 ){
+//     alert("the given number is positive");
+// }
+// else if(num %2 != 2){
+//     alert("the give number is negative");
+// }
+// else{
+//     alert("the given number is zero");
+// }
